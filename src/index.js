66b3668fcf73959a9b91c95b231728315f85bd33
Rogier Slag <rogier.slag@gmail.com>
2017-12-14 12:20:28 +0100

    Add polyfills

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import 'core-js/es6/set';
+import 'core-js/es6/map';
+import 'core-js/fn/set';
+import 'core-js/fn/map';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 
 ReactDOM.render(<App/>, document.getElementById('root'));
