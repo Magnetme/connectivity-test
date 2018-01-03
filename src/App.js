42bb6417d61cf85f52f55fb9c5b2127fbfb8d9b8
Rogier Slag <rogier.slag@gmail.com>
2018-01-03 10:05:45 +0100

    Add browser information

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, {PureComponent} from 'react';
+import platform from 'platform';
 import './App.css';
 import Header from './Header';
 import TestItemList from "./TestItemList";
 import tests from './tests';
+
+const Line = <p className="line"/>
 
 class App extends PureComponent {
 	render() {
@@ -13,6 +16,16 @@ class App extends PureComponent {
 					This system will determine whether there are any connectivity issues between your computer and the Magnet.me
 					network.
 				</p>
+				<p>
+					The test consists of two separate parts.
+					<ol>
+						<li>The first part will automatically persorm several connectivity checks.</li>
+						<li>The second part includes information about the system you are using (no personal information will be included).</li>
+					</ol>
+				</p>
+
+				{Line}
+
 				<p className="App-explanation">
 					After taking note of the displayed test results, please inform your account manager or supporter of the
 					results below.
@@ -25,6 +38,15 @@ class App extends PureComponent {
 					In case the your network does not support IPv6 the IPv6 tests can fail.
 					This does not indicate a problem as long as the IPv4 tests succeed.
 				</p>
+
+				{Line}
+
+				<p>
+					The following information will tell our engineers more about the system you are using.
+					Please copy/paste the text below in your email to your account manager or supporter.
+				</p>
+
+				<pre className="browser"><code>{JSON.stringify(platform, null, 4)}</code></pre>
 			</div>
 		);
 	}
