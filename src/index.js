a256c5d1d1648ef21da34632798b98f9fd4132da
Rogier Slag <rogier.slag@gmail.com>
2019-06-12 10:08:51 +0200

    Update the repo to use more recent packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import 'core-js/es6/set';
-import 'core-js/es6/map';
-import 'core-js/fn/set';
-import 'core-js/fn/map';
+import 'core-js/features/set';
+import 'core-js/features/map';
+import 'core-js/es/set';
+import 'core-js/es/map';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
