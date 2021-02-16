1a1949a272327e391ba524c3809db6bd0aedf380
Rogier Slag <rogier.slag@gmail.com>
2021-02-16 16:24:31 +0100

    Update connectovity test

--- src/Environment.js
+++ src/Environment.js
@@ -1,11 +1,11 @@
-import React, {PureComponent, Fragment} from "react";
+import React, {Fragment, PureComponent} from "react";
 import Device from 'react-device';
 
 import './Environment.css';
 
 function testStorage(storageReference) {
 	const mod = 'testStorage';
-	return (function() {
+	return (function () {
 		try {
 			storageReference.setItem(mod, mod);
 			storageReference.removeItem(mod);
@@ -22,37 +22,41 @@ class Environment extends PureComponent {
 		super(props);
 		this.state = {
 			ip : {},
+			userAgent : {},
 		};
 		this.onChange = this.onChange.bind(this);
 	}
 
 	async componentDidMount() {
-		const result = await fetch('http://api.ipstack.com/check?hostname=1&access_key=c7de533b6bb95365e6f4efc3779e17de');
-		const json = await result.json();
-		console.log('New IP data received');
-		this.setState({ip : json});
+		fetch('http://api.ipstack.com/check?hostname=1&access_key=c7de533b6bb95365e6f4efc3779e17de')
+			.then(response => response.json())
+			.then(ip => this.setState({ip}));
+
+		fetch('https://clients.magnet.me/user_agent')
+			.then(response => response.json())
+			.then(userAgent => this.setState({userAgent}));
 	}
 
 	onChange(data) {
 		console.log('New device data received');
 		const browser = {
 			...data.browser,
-			browserOnline: window.navigator.onLine,
-			javaEnabled: window.navigator.javaEnabled(),
-			dataCookiesEnabled: window.navigator.cookieEnabled,
-			localStorageEnabled: testStorage(window.localStorage),
-			sessionStorageEnabled: testStorage(window.sessionStorage),
+			browserOnline : window.navigator.onLine,
+			javaEnabled : window.navigator.javaEnabled(),
+			dataCookiesEnabled : window.navigator.cookieEnabled,
+			localStorageEnabled : testStorage(window.localStorage),
+			sessionStorageEnabled : testStorage(window.sessionStorage),
 		};
 		this.setState(Object.assign({}, data, {browser}));
 	}
 
 	render() {
 		return <Fragment>
-			<Device onChange={this.onChange} />
+			<Device onChange={this.onChange}/>
 			<pre className="environment">
 			<code>
 				{JSON.stringify(this.state, null, 4)}
-				</code>
+			</code>
 		</pre>
 		</Fragment>;
 	}
